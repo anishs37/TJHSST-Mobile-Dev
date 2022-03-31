3a4edea0d74a9454f0f451c9feacdabeffaf934c
Anish Susarla <47859534+anishs37@users.noreply.github.com>
2022-03-31 16:44:13 -0400

    lab07a + lab08 part 1

---
+++ Lab07/app/src/main/java/com/susarlaanish/lab07/FragmentB.java
@@ -0,0 +1,56 @@
+package com.susarlaanish.lab07;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentB extends Fragment {
+    View view;
+
+    // Creates a new fragment given an int and title
+    public static FragmentB newInstance(int someInt, String someTitle) {
+        FragmentB fragmentB = new FragmentB();
+        Bundle args = new Bundle();//package variables into Bundle
+        args.putInt("someInt", someInt);
+        args.putString("someTitle", someTitle);
+        fragmentB.setArguments(args);//assign bundle to arguments
+        return fragmentB;
+    }
+
+    //View inflation/instantiation
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b, container,false);
+    }
+    // Setup requiring access to View starts here
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.view = view;
+
+        // Get back arguments
+        int someInt = getArguments().getInt("someInt", 0);
+        String someTitle = getArguments().getString("someTitle", "");
+
+        //setup here
+        TextView textView = view.findViewById(R.id.b_textview);
+        textView.setText(someTitle);
+        Button button = view.findViewById(R.id.b_button);
+        String count = getString(R.string.count, someInt);
+        button.setText(count);
+    }
+
+    // Parent Activity has finished loading, access parent views here
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
---
+++ Lab07/app/src/main/java/com/susarlaanish/lab07/MainActivity.java
@@ -0,0 +1,45 @@
+package com.susarlaanish.lab07;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        // Replace the contents of the container with the new fragment
+        ft.add(R.id.fragment_container, FragmentB.newInstance(4,"Hello SuperStar"),"FragmentName");
+        //or ft.replace(R.id.fragment_container, new FragmentB());
+
+        // Complete the changes added above
+        ft.commit();
+    }
+
+    public void update_greeting(View view) {
+        //access the fragment by tag
+        FragmentB fragmentB = (FragmentB)getSupportFragmentManager().findFragmentByTag("FragmentName");
+        if(fragmentB!=null)
+            ((TextView)fragmentB.view.findViewById(R.id.b_textview)).setText(R.string.greeting2);
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(R.animator.slide_up,R.animator.slide_down);
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.fragment_container, FragmentB.newInstance(3,"Hello RockStar"),"BetterFragment");
+        // Complete the changes added above
+        ft.commit();
+    }
+
+    public void fragment_button_onclick(View view) {
+        ((Button)view).setText(R.string.clicked_message);
+    }
+}
