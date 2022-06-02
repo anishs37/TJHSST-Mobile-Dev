d209a60540b4744620919eb65f56a1dfd818d5fd
Anish Susarla <47859534+anishs37@users.noreply.github.com>
2022-06-01 22:04:48 -0400

    final part 1 + lab10

---
+++ Final/app/src/main/java/com/susarlaanish/afinal/MainActivity.java
@@ -0,0 +1,65 @@
+package com.susarlaanish.afinal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.*;
+import com.google.firebase.auth.*;
+import com.google.firebase.database.*;
+
+/Users/anish/Documents/TJHSST-AI-2/Unit 9/Back Propagation/mnist_test.py
+        /Users/anish/Documents/TJHSST-AI-2/Unit 8/KMeans/image_vec_dither.py
+        /Users/anish/Documents/TJHSST-AI-2/Unit 9/Gradient Descent/line_optimization.py
+        /Users/anish/Documents/TJHSST-AI-2/Unit 8/KMeans/image_vec.py
+        /Users/anish/Documents/TJHSST-AI-2/Unit 8/KMeans/copilot_test.py
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword("abc@abc.com", "test123!")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(MainActivity.this, user.getEmail(),
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(MainActivity.this, "Authentication failed." +task.getException(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println("Firebase test: Value is: " + value);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                System.out.println("Failed to read value."+ databaseError.toException());
+            }
+        });
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Mobile Application Development
-
-This repository will contain my code for the Mobile Application Development labs at TJHSST.
